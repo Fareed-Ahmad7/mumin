2effa3dace51090758d7a015b7fb5bbfd2f291f5
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-07-31 20:00:50 +0530

    started kotlin app

---
+++ app/src/main/java/com/fareedahmad1097/mumin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fareedahmad1097.mumin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
